cddfd48766b14654176a09c12298b60eb8fccfab
wjykk <no@no>
2021-03-13 21:23:34 -0700

    Add provisioning script for Windows

---
+++ provision.ps1
@@ -0,0 +1,57 @@
+# Run this script by opening PowerShell as administrator and run
+#   Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://yakov.ca/provision.ps1'))
+
+#Requires -RunAsAdministrator
+
+$apps = @{
+    Home = @(
+        "7zip",
+        "microsoft-teams",
+        "office365business"
+        "opera",
+        "skype",
+        "viber",
+        "vlc"
+        # TODO WeatherEye
+    );
+    Yakov = @(
+        "7zip",
+        "8gadgetpack",
+        "adoptopenjdk",
+        "brave",
+        "cmake",
+        "discord",
+        "docker-desktop",
+        "foxitreader",
+        "gitextensions",
+        "intellijidea-ultimate",
+        "paint.net"
+        "putty",
+        "python",
+        "steam-client",
+        "transmission",
+        "visualstudio2019buildtools",
+        "vlc",
+        "vscode",
+        "windirstat",
+        "xming"
+    );
+    YakovSED = @(
+        "microsoft-teams",
+        "mtputty",
+        "openvpn",
+        "slack"
+    );
+}
+
+$selection = ""
+while (-not($apps.Contains($selection))) {
+    $selection = Read-Host "Enter profile name: ($($apps.Keys))"
+}
+
+$choco = Get-Command -Name notepad.exe -ErrorAction SilentlyContinue
+if (-not($choco)) {
+    Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+}
+
+&choco install -y $apps[$selection]
